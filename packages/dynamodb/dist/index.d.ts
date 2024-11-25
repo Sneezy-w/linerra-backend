8ce482faf23608bf884077a2d67932427071fb55
Hamilton Wang <hamilton.y.wang@gmail.com>
2024-11-25 01:50:51 -0600

    Enhance DynamoDB package structure and build scripts
    - Added new build scripts for DynamoDB package in package.json to streamline development processes.
    - Updated TypeScript configuration to include declaration and source maps for better type support and debugging.
    - Removed obsolete files and repositories related to shipment and user session management to clean up the codebase.
    - Refactored the DynamoDB entity and repository structure to improve maintainability and organization.
    
    These changes improve the development workflow and enhance the overall project structure.

--- packages/dynamodb/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * as stationUserSessionRepository from './repository/stationUserSessionRepository';
-export * as shipmentRepository from './repository/shipmentRepository';
--- packages/dynamodb/dist/utils/dynamoClient.d.ts
+++
@@ -1,4 +0,0 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-export declare const dynamoClient: DynamoDBClient;
-export declare const documentClient: DynamoDBDocumentClient;
